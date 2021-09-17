cb237218c543e9e9c609b91f5c65b99ed42188c8
anirban-baisya <anirban.baisya2020@vitbhopal.ac.in>
2021-09-17 17:49:12 +0530

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,15 @@ let currentTeam = "Manchester FC";
 let trophiesWon = 27;
 
 //Write your function here
+function createManager(managerName, managerAge, currentTeam, trophiesWon) {
+  var managerArray = [managerName,
+   managerAge  ,
+    currentTeam ,
+ trophiesWon]
+ return managerArray;
+}
+
+
 
 // Don't edit the following code
 try {
@@ -22,6 +31,15 @@ try {
 var formation = [4, 4, 3];
 
 //write your function here
+function createFormation(formation) {
+  if (formation.length== 0) {
+    return null;
+  } else {
+    var formate = { defender:formation[0], midfield:formation[1], forward:formation[2]}; //obj
+  }
+  return formate;
+}
+
 
 // Dont edit the following code
 
@@ -32,8 +50,33 @@ try {
 }
 
 //Progression 3 - Filter players that debuted in ___ year
+function filterByDebut(year) {
+  var playerList= [];
+  let j = 0;
+  for (let i of players) {
+    if(i.debut==year){
+      playerList[j]=i;
+      j++;
+    }
+  }
+  return playerList;
+}
+
 
 //Progression 4 - Filter players that play at the position _______
+
+function filterByPosition(position) {
+  var playerList= []; 
+  let j = 0;
+  for(let i of players ){
+    if(i.pos== position){
+      playerList[j]= i;
+    j++;
+    }
+  }
+  return playerList;
+}
+
 
 //Progression 5 - Filter players that have won ______ award
 
@@ -44,6 +87,11 @@ try {
 //Progression 8 - Filter players that won atleast ______ awards, belong to ______ team and are younger than ____
 
 //Progression 9 - Sort players in descending order of their age
+function SortByAge() {
+  var playersList = [];
+  playersList = Object.age(players).sort()
+  return playersList.reverse();
+}
 
 //Progression 10 - Sort players beloging to _____ team in descending order of awards won
 
